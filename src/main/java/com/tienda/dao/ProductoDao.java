a4019a6bce369832cc718b3000817855b9645ac6
Johan Sánchez Vásquez <johannsava09@gmail.com>
2025-07-25 00:41:20 -0600

    Commit de clase 11

--- src/main/java/com/tienda/dao/ProductoDao.java
+++ src/main/java/com/tienda/dao/ProductoDao.java
@@ -6,10 +6,25 @@
 package com.tienda.dao;
 import com.tienda.domain.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;     
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 /**
  *
  * @author johan
  */
 public interface ProductoDao extends JpaRepository <Producto, Long> {
+    
+    //Consulta ampliada
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+    
+    //Consulta tipo JPQL (mix entre Java y SQL)
+    @Query(value="SELECT a FROM Producto a where a.precio BETWEEN :precioInf AND :precioSup ORDER BY a.descripcion ASC")
+    public List<Producto> metodoJPQL(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
+    
+    //Ejemplo de método utilizando consultas con SQL nativo
+    @Query(nativeQuery=true,
+            value="SELECT * FROM producto where producto.precio BETWEEN :precioInf AND :precioSup ORDER BY producto.descripcion ASC")
+    public List<Producto> metodoNativo(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
 
 }
